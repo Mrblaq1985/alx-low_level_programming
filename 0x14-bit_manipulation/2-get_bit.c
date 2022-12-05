ee75fb810b30a6d6b31aa7f28371d2463e23874a
Utibe <utibemfon45@gmail.com>
2022-12-05 01:32:07 -0800

    Bug fix1

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "holberton.h"
 
 /**
  * get_bit - returns the value of a bit at a given index.
@@ -10,13 +9,18 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int divisor, check;
+	unsigned int i;
 
-	if (index > (sizeof(unsigned long int) * 8 - 1))
-		return (-1);
-	divisor = 1 << index;
-	check = n & divisor;
-	if (check == divisor)
-		return (1);
-	return (0);
+	if (n == 0 && index < 64)
+		return (0);
+
+	for (i = 0; i <= 63; n >>= 1, i++)
+	{
+		if (index == i)
+		{
+			return (n & 1);
+		}
+	}
+
+	return (-1);
 }
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "holberton.h"
 
 /**
  * set_bit - sets the value of a bit to 1 at a given index.
@@ -10,11 +9,13 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int set;
+	unsigned int m;
 
-	if (index > (sizeof(unsigned long int) * 8 - 1))
+	if (index > 63)
 		return (-1);
-	set = 1 << index;
-	*n = *n | set;
+
+	m = 1 << index;
+	*n = (*n | m);
+
 	return (1);
 }
